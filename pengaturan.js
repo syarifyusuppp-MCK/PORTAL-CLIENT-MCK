05df40b7cbdd85bf95cdb0fa689f9f1b167eef2c
syarifyusuppp-MCK <syarifyusuppp@gmail.com>
2026-09-07 15:32:49 +0700

    Update pengaturan.js

--- pengaturan.js
+++ pengaturan.js
@@ -9,5 +9,5 @@ const CONFIG_APP = {
 
     // 2. Google Drive API Key (Penting agar galeri bisa memuat foto)
     googleApiKey: "AIzaSyAZczbLeVTXl-QKqLDJFWQLCd-lW9jqqxo",
-    whatsappAdmin: "62821000000000" // <-- TAMBAHKAN BARIS INI (Ganti dengan nomor WA admin)
+    whatsappAdmin: "6285389189250" // <-- TAMBAHKAN BARIS INI (Ganti dengan nomor WA admin)
 };
